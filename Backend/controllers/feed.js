bca1709fd4ce582a5edefae314afbd4e8c6aba0b
Shubham Bhardwaj <shubham998845@gmail.com>
2023-01-03 16:22:18 +0530

    server side validation

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,15 +1,41 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "first", content: "first content" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "First content",
+        imageUrl: "images/jerry.png",
+        creator: {
+          name: "Shubh",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: "Validation failed", errors: errors.array() });
+  }
   const title = req.body.title;
+
   const content = req.body.content;
   //createpost in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().getTime(), title: title, content: content },
+    post: {
+      _id: new Date().getTime(),
+      title: title,
+      content: content,
+      creator: { name: "Shubham" },
+      createdAt: new Date(),
+    },
   });
 };
