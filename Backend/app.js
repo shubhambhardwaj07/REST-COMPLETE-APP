ff6fb5cf8749205dfd24164101af7a014433eb53
Shubham Bhardwaj <shubham998845@gmail.com>
2023-01-06 16:00:51 +0530

    modelongoose added, static handling of images and error middleware added

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,15 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const moongoose = require("mongoose");
 const feedRoutes = require("./routes/feed");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 //app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form></form>
 app.use(bodyParser.json()); //application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,5 +19,19 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://shubham-node:Shubham%40123@cluster0.mnmqbyh.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log("Connected advance rest");
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -20,22 +22,33 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "Validation failed", errors: errors.array() });
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
 
   const content = req.body.content;
   //createpost in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().getTime(),
-      title: title,
-      content: content,
-      creator: { name: "Shubham" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/jerry.png",
+    creator: { name: "Shubham" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
