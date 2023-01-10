9183b978525529713a0852f2bec1c45f015652aa
Shubham Bhardwaj <shubham998845@gmail.com>
2023-01-10 15:40:35 +0530

    pagination

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -4,6 +4,30 @@ const path = require("path");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
+    .then((posts) => {
+      res.status(200).json({
+        message: "fetched posts successfuly",
+        posts: posts,
+        totalItems: totalItems,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
   Post.find()
     .then((posts) => {
       res.status(200).json({
