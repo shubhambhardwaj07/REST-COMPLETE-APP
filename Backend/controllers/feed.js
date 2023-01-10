c31ac14e0c20252c65c2406400dffded80d0fc95
Shubham Bhardwaj <shubham998845@gmail.com>
2023-01-10 13:48:21 +0530

    upload image

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -25,6 +25,13 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\", "/");
   const title = req.body.title;
 
   const content = req.body.content;
@@ -32,7 +39,7 @@ exports.createPost = (req, res, next) => {
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/jerry.png",
+    imageUrl: imageUrl,
     creator: { name: "Shubham" },
   });
   post
